aff5f86727f283a290e8fbafc540266190a58835
nekosenko <brija.main@gmail.com>
2021-06-09 17:41:06 +0100

    parsing

--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -6,18 +6,36 @@
 /*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 11:30:44 by mbrija            #+#    #+#             */
-/*   Updated: 2021/06/09 13:21:13 by mbrija           ###   ########.fr       */
+/*   Updated: 2021/06/09 14:53:04 by mbrija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
+int	collect_data(int ac, char **av)
+{
+	g_conf.nbr_p = ft_atoi(av[1], 0);
+	g_conf.t_die = ft_atoi(av[2], 0) * 1000;
+	g_conf.t_eat = ft_atoi(av[3], 0) * 1000;
+	g_conf.t_sleep = ft_atoi(av[4], 0) * 1000;
+	g_conf.nbr_end = -1;
+	if (ac == 6)
+		g_conf.nbr_end = ft_atoi(av[5], 0);
+	if (g_conf.nbr_p <= 0 || g_conf.t_die <= 0
+		|| g_conf.t_die <= 0 || g_conf.t_sleep <= 0
+		|| (ac == 6 && g_conf.nbr_end <= 0))
+		return (senko_err("Error: Unvalid Argument\n"));
+	return (EXIT_SUCCESS);
+}
+
 int	main(int ac, char **av)
 {
 	(void)av;
 	if (ac != 5 && ac != 6)
-		return (senko_err("Error: Argument\n"));
+		return (senko_err("Error: Missing or too many arguments\n"));
 	//collect data
-	//init and unlock mutex
+	if (collect_data(ac, av))
+		return (EXIT_FAILURE);
+	//init and unlock mutex out
 	return (EXIT_SUCCESS);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -6,7 +6,7 @@
 /*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 14:21:52 by mbrija            #+#    #+#             */
-/*   Updated: 2021/06/09 13:20:05 by mbrija           ###   ########.fr       */
+/*   Updated: 2021/06/09 14:51:00 by mbrija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,43 @@
 # define LEAVINGFORKS 7
 # define DONE 8
 
+typedef long	t_micro_s_t;
+
+typedef struct s_conf
+{
+	int				nbr_p;
+	int				run;
+	t_micro_s_t		t_die;
+	t_micro_s_t		t_eat;
+	t_micro_s_t		t_sleep;
+	int				nbr_end;
+	pthread_mutex_t	mutex;
+	pthread_mutex_t	mutex_out;
+	pthread_mutex_t	death;
+}				t_conf;
+
+typedef struct s_philosopher
+{
+	int			id;
+	int			status;
+	int			total_eated;
+	int			is_dead;
+	pthread_t	philo_th;
+	t_micro_s_t	t_last_eat;
+	t_micro_s_t	start_sleep;
+}				t_philosopher;
+
+pthread_mutex_t	*g_forks;
+t_philosopher	*g_philosophers;
+t_micro_s_t		g_time_start;
+t_conf			g_conf;
+
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr(int n);
 void	ft_putstr_fd(char *s, int fd);
 int		ft_strlen(char *s);
 int		ft_atoi(const char *str, int i);
 int		senko_err(char *error);
+int		collect_data(int ac, char **av);
 
 #endif
