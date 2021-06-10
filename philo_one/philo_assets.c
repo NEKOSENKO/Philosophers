6ee822aff4cf2f986721f448d7d96f4c2d94f674
nekosenko <brija.main@gmail.com>
2021-06-10 15:21:07 +0100

    setting threads .. next is sync

--- philo_one/philo_assets.c
+++ philo_one/philo_assets.c
@@ -6,7 +6,7 @@
 /*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 13:03:07 by mbrija            #+#    #+#             */
-/*   Updated: 2021/06/09 13:20:20 by mbrija           ###   ########.fr       */
+/*   Updated: 2021/06/10 12:20:48 by mbrija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,12 @@ int	senko_err(char *error)
 {
 	write(2, error, ft_strlen(error));
 	return (EXIT_FAILURE);
+}
+
+t_micro_s_t get_time_stamp()
+{
+	struct timeval	time_v;
+	
+	gettimeofday(&time_v, NULL);
+	return (time_v.tv_sec * (t_micro_s_t)1000000 + time_v.tv_usec);
 }
--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -6,7 +6,7 @@
 /*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 11:30:44 by mbrija            #+#    #+#             */
-/*   Updated: 2021/06/09 14:53:04 by mbrija           ###   ########.fr       */
+/*   Updated: 2021/06/10 12:31:43 by mbrija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,50 @@ int	collect_data(int ac, char **av)
 	return (EXIT_SUCCESS);
 }
 
+int phil_init_next()
+{
+	int i;
+
+	i = 0;
+	while (i < g_conf.nbr_p)
+	{
+		g_philosophers[i].id = i + 1;
+		g_philosophers[i].is_dead = FALSE;
+		g_philosophers[i].status = THINKING;
+		g_philosophers[i].t_last_eat = g_time_start;
+		if (pthread_create(&g_philosophers[i].philo_th, NULL, &function,
+		g_philosophers[i]))
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	phil_init()
+{
+	int i;
+
+	g_time_start = get_time_stamp();
+	g_conf.run = TRUE;
+	g_forks = (pthread_mutex_t *)malloc((g_conf.nbr_p * sizeof(pthread_mutex_t)));
+	if (!g_forks)
+		return (EXIT_FAILURE);
+	if (pthread_mutex_init(&g_conf.mutex, NULL)
+		|| pthread_mutex_init(&g_conf.mutex_out, NULL)
+		|| pthread_mutex_init(&g_conf.death, NULL))
+		return (EXIT_FAILURE);
+	i = 0;
+	while (i < g_conf.nbr_p)
+		if (pthread_mutex_init(&g_forks[i++], NULL))
+			return (EXIT_FAILURE);
+	g_philosophers = malloc(g_conf.nbr_p * sizeof(t_philosopher));
+	if (!g_philosophers)
+		return (EXIT_FAILURE);
+	if (phil_init_next())
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
 int	main(int ac, char **av)
 {
 	(void)av;
@@ -37,5 +81,10 @@ int	main(int ac, char **av)
 	if (collect_data(ac, av))
 		return (EXIT_FAILURE);
 	//init and unlock mutex out
-	return (EXIT_SUCCESS);
+	if (phil_init())
+	{
+		pthread_mutex_unlock(&g_conf.mutex_out);
+		return (EXIT_FAILURE);
+	}
+	// return (EXIT_SUCCESS);
 }
